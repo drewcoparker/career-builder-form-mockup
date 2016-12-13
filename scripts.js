00a63d63e6b32573628cbfe5fe929bb4803f86b0
Drew Parker <drew.i.parker@gmail.com>
2016-12-12 19:02:27 -0500

    Started to implement form submit rules (i.e., cannot be empty, etc.)

--- scripts.js
+++ scripts.js
@@ -1,3 +1,22 @@
+//Jquery
+
 $(document).ready(function() {
+
+    // Prevent a the form from refreshing page
+    $('form').submit(function() {
+        event.preventDefault();
+
+        $('input').each(function() {
+            var currentTagId = $(this).attr('id');
+            var errorClass = '.' + currentTagId + '-error';
+            console.log(errorClass);
+            if ($(this).val() === '') {
+                $(errorClass).html("Field cannot be empty.");
+                $(errorClass).show();
+            } else {
+                $(errorClass).hide();
+            }
+        });
+    });
 
 });
